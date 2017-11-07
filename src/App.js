786b671ad9f6d6c37b5fb805b299d203af606aa0
Luís Miguel <luism@marzeelabs.org>
2017-11-07 17:09:17 +0000

    add semester dropdown for easy datepicker debugging

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 
 import DatePicker from './DatePicker';
 
-class App extends Component {
-  render() {
-    const period = {
+const dates = {
+  1: {
+    period: {
       startYear: 2017,
       endYear: 2018,
       semester: 1
-    };
-
-    const availability = {
+    },
+    availability: {
       '8 sy': true,
       '9 sy': true,
       '10 sy': true,
@@ -26,7 +25,49 @@ class App extends Component {
       '6 ey': true,
       '7 ey': false,
       '8 ey': false
+    }
+  },
+  2: {
+    period: {
+      startYear: 2017,
+      endYear: 2018,
+      semester: 2
+    },
+    availability: {
+      '8 sy': true,
+      '9 sy': true,
+      '10 sy': true,
+      '11 sy': true,
+      '12 sy': true,
+      '1 ey': true,
+      '2 ey': true,
+      '3 ey': true,
+      '4 ey': true,
+      '5 ey': true,
+      '6 ey': true,
+      '7 ey': true,
+      '8 ey': true
+    }
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: 1
     };
+
+    this.changeDate = this.changeDate.bind(this);
+  }
+
+  changeDate(event) {
+    this.setState({ date: event.target.value });
+  }
+
+  render() {
+    const { period, availability } = dates[this.state.date];
 
     return (
       <div className="App">
@@ -34,6 +75,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">uHub Reservations</h1>
         </header>
+
+        <select onChange={this.changeDate} value={this.state.date}>
+          <option value="1">1st Sem 17/18</option>
+          <option value="2">2nd Sem 17/18</option>
+        </select>
 
         <DatePicker
           period={period}
